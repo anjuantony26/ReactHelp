8acd94e21881f21035e2b4ccf48238eb8c4638bf
George Tony <gtony@suyati.com>
2016-10-27 11:29:03 +0530

    Google Charts

--- src/googlePie.js
+++ src/googlePie.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
 export class GooglePie extends Component {
-
   componentDidMount(){
-    let google = null
-    console.log(google,"before timeout")
-  setTimeout( () => {
-    console.log(google,"timeout")
-    console.log(google.charts,document.getElementById('Pie'))
-     google.charts.load('current', {'packages':['corechart']});
-       google.charts.setOnLoadCallback(drawChart);
-       function drawChart() {
+    this.drawCharts();
+  }
+  componentDidUpdate(){
+    this.drawCharts();
+  }
+  /*global someFunction google:true*/
+  /*
+  ESLint to take google as global varible
+  */
+  drawCharts(){
+    var data = google.visualization.arrayToDataTable([
+         ['Task', 'Hours per Day'],
+         ['Work',     11],
+         ['Eat',      2],
+         ['Commute',  2],
+         ['Watch TV', 2],
+         ['Sleep',    7]
+       ]);
 
-         var data = google.visualization.arrayToDataTable([
-           ['Task', 'Hours per Day'],
-           ['Work',     11],
-           ['Eat',      2],
-           ['Commute',  2],
-           ['Watch TV', 2],
-           ['Sleep',    7]
-         ]);
+       var options = {
+         title: 'My Daily Activities'
+       };
 
-         var options = {
-           title: 'My Daily Activities'
-         };
+       var chart = new google.visualization.PieChart(document.getElementById('Pie'));
 
-         var chart = new google.visualization.PieChart(document.getElementById('Pie'));
-
-         chart.draw(data, options);
-       }
-  },3000)
+       chart.draw(data, options);
   }
   render(){
     return (
